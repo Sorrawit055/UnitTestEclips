e240087e396b0174e5d2b1399a99b351379dc5ba
Sorrawit Eaknarongpan <614259055@webmail.npru.ac.th>
2021-03-23 20:01:38 +0700

    Eclipse

--- Calendar.php
+++ Calendar.php
@@ -3,7 +3,6 @@ class Calendar{
 public function showday($date,$month){ 
 $day = "2021-$month-$date";
 $dayOfWeek = date("l", strtotime($day));
-echo $day . ' เป็นวัน ';
 return $dayOfWeek;
     }
 }
